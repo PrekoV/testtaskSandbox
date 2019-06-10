1791164483660b7fe7d916accc5cd02cde85f308
Anton L <antonolya888@gmail.com>
2019-06-10 15:01:47 +0300

    fix more logic bags

--- src/App.js
+++ src/App.js
@@ -9,20 +9,26 @@ function App() {
 
     const addSublist = (e, id) => {
         e.preventDefault();
-        let newList = [...list];
-        let findDeep = function(data, id) {
-            return data.some(function(item) {
-                if (item.id === id) {
-                    item.sublist.push({
-                        id: randomId(),
-                        name: e.target.input.value,
-                        sublist: []
-                    });
-                    return true;
-                } else if (item.sublist) return findDeep(item.sublist, id);
-            });
-        };
-        findDeep(newList, id) && setList(newList)
+        if (e.target.input.value !== "") {
+            let newList = [...list];
+            let findDeep = function(data, id) {
+                return data.some(function(item) {
+                    if (item.id === id) {
+                        console.log(id, data);
+                        item.sublist.push({
+                            id: randomId(),
+                            name: e.target.input.value,
+                            sublist: []
+                        });
+                        return true;
+                    } else if (item.sublist) return findDeep(item.sublist, id);
+                });
+            };
+            findDeep(newList, id) && setList(newList);
+            console.log(list);
+            e.target.input.value = "";
+        }
+
         // if (findDeep(newList, id)) {
         //     console.log(newItem)
         //     newItem[0][0].sublist.push({
@@ -36,24 +42,27 @@ function App() {
 
     const add = e => {
         e.preventDefault();
-        let arr = [...list];
-        let newObj = {
-            id: list.length,
-            name: e.target.input.value,
-            sublist: []
-        };
-        arr.push(newObj);
-        setList(arr);
+        if (e.target.input.value !== "") {
+            let arr = [...list];
+            let newObj = {
+                id: randomId(),
+                name: e.target.input.value,
+                sublist: []
+            };
+            arr.push(newObj);
+            setList(arr);
+            e.target.input.value = "";
+        }
     };
 
     const remove = id => {
-        console.log(id)
+        console.log(id);
         let newList = [...list];
-        let a = []
+        let a = [];
         let findDeep = function(data, id) {
             return data.some(function(item) {
                 if (item.id === id) {
-                    a.push(item)
+                    a.push(item);
                     data.splice(data.indexOf(item), 1);
                 } else if (item.sublist) return findDeep(item.sublist, id);
             });
@@ -63,52 +72,51 @@ function App() {
     };
 
     const up = (id, index) => {
-        let newList = [...list]
+        let newList = [...list];
         let findDeep = function(data, id) {
             return data.some(function(item) {
                 if (item.id === id) {
-                    let a = {...data[index-1]}
-                    data[index-1] = data[index]
-                    data[index] = a
+                    let a = { ...data[index - 1] };
+                    data[index - 1] = { ...data[index] };
+                    data[index] = { ...a };
                     return true;
                 } else if (item.sublist) return findDeep(item.sublist, id);
             });
         };
         findDeep(newList, id);
-        setList(newList)
-    }
+        setList(newList);
+    };
 
     const down = (id, index) => {
-        let newList = [...list]
+        let newList = [...list];
         let findDeep = function(data, id) {
             return data.some(function(item) {
                 if (item.id === id) {
-                    let a = {...data[index+1]}
-                    data[index+1] = data[index]
-                    data[index] = a
+                    let a = { ...data[index + 1] };
+                    data[index + 1] = { ...data[index] };
+                    data[index] = { ...a };
                     return true;
                 } else if (item.sublist) return findDeep(item.sublist, id);
             });
         };
         findDeep(newList, id);
-        setList(newList)
-    }
+        setList(newList);
+    };
 
-    const remoteSublist = (id) => {
-        
-        let newList = [...list]
+    const remoteSublist = id => {
+        let newList = [...list];
         let findDeep = function(data, id) {
             return data.some(function(item) {
                 if (item.id === id) {
-                    item.sublist=[]
+                    item.sublist = [];
                     return true;
                 } else if (item.sublist) return findDeep(item.sublist, id);
             });
         };
         findDeep(newList, id);
-        setList(newList)
-    }
-
+        setList(newList);
+    };
+    console.log(list);
     return (
         <div className="App">
             <ul>
@@ -120,10 +128,10 @@ function App() {
                         addSublist={addSublist}
                         setActive={setActive}
                         active={active}
-                        next={item.id}
+                        itemId={item.id}
                         remove={remove}
                         index={index}
-                        parentArrayLength = {list.length}
+                        parentArrayLength={list.length}
                         up={up}
                         down={down}
                         remoteSublist={remoteSublist}
@@ -142,23 +150,68 @@ function App() {
 
 export default App;
 
-function Item({ name, sublist, addSublist, active, setActive, next, remove, index, parentArrayLength, up, down, remoteSublist }) {
-    let next1;
+function Item({
+    name,
+    sublist,
+    addSublist,
+    active,
+    setActive,
+    itemId,
+    remove,
+    index,
+    parentArrayLength,
+    up,
+    down,
+    remoteSublist
+}) {
+    // let currentId;
     return (
         <li>
             <div>
                 {name}
-                { parentArrayLength - 1 === index && parentArrayLength !== 1 && <button onClick={() => up(next, index)} >&uarr;</button>}
-                { index === 0  && parentArrayLength !== 1 && <button onClick={() => down(next, index)} >&darr;</button>}
-                { parentArrayLength !== 1 && index !== 0 && parentArrayLength - 1 !== index && <> <button  onClick={() => up(next, index)} >&uarr;</button> <button onClick={() => down(next, index)} >&darr;</button> </>}
-                { sublist.length!==0 ? <button onClick={() => remoteSublist(next)}>Remote Sublist</button> : <button onClick={() => setActive(next)}> Add Sublist</button>}
-                
-                <button onClick={() => {remove(next); setActive(null)}}>Remove</button>
+                {parentArrayLength - 1 === index && parentArrayLength !== 1 && (
+                    <button onClick={() => up(itemId, index)}>&uarr;</button>
+                )}
+                {index === 0 && parentArrayLength !== 1 && (
+                    <button onClick={() => down(itemId, index)}>&darr;</button>
+                )}
+                {parentArrayLength !== 1 &&
+                    index !== 0 &&
+                    parentArrayLength - 1 !== index && (
+                        <>
+                            {" "}
+                            <button onClick={() => up(itemId, index)}>
+                                &uarr;
+                            </button>{" "}
+                            <button onClick={() => down(itemId, index)}>
+                                &darr;
+                            </button>{" "}
+                        </>
+                    )}
+                {sublist.length !== 0 ? (
+                    <button onClick={() => remoteSublist(itemId)}>
+                        Remote Sublist
+                    </button>
+                ) : (
+                    <button onClick={() => setActive(itemId)}>
+                        {" "}
+                        Add Sublist
+                    </button>
+                )}
+
+                <button
+                    onClick={() => {
+                        remove(itemId);
+                        setActive(null);
+                    }}
+                >
+                    Remove
+                </button>
             </div>
             <ul style={{ margin: "5px 25px" }}>
                 {sublist !== null &&
                     sublist.map((item, index) => {
-                        next1 = item.id;
+                        // next1 = item.id;
                         return (
                             <Item
                                 key={item.id}
@@ -166,19 +219,28 @@ function Item({ name, sublist, addSublist, active, setActive, next, remove, inde
                                 addSublist={addSublist}
                                 setActive={setActive}
                                 active={active}
-                                next={next1}
+                                itemId={item.id}
                                 remove={remove}
-                                index = {index}
+                                index={index}
                                 parentArrayLength={sublist.length}
                                 up={up}
-                                down = {down}
-                                remoteSublist = {remoteSublist}
+                                down={down}
+                                remoteSublist={remoteSublist}
                             />
                         );
                     })}
-                {(active === next || sublist.length !==0) && (
-                    <form onSubmit={e => addSublist(e, active)}>
-                        <input name="input" type="text" />
+                {(active === itemId || sublist.length !== 0) && (
+                    <form
+                        onSubmit={e => {
+                            addSublist(e, active);
+                            setActive("");
+                        }}
+                    >
+                        <input
+                            onFocus={() => setActive(itemId)}
+                            name="input"
+                            type="text"
+                        />
                         <button>Add</button>
                     </form>
                 )}
